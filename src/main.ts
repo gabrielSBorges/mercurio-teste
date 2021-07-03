65682992e9912d324ec059f5f558dfbfd2dae036
gabrielSBorges <borgesgabrielsilva@gmail.com>
2021-07-03 03:36:31 -0300

    teste

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,10 @@
 import { Client } from 'discord.js';
 import dotenv from 'dotenv';
-import { createServer } from 'http';
 
 dotenv.config();
 
 const client = new Client();
 const token = process.env.BOT_TOKEN;
-
-createServer((req, res) => {
-	
-	res.writeHead(200, {
-		'Content-type': 'text/plain'
-	});
-
-	res.write('Hey');
-	res.end();
-}).listen(4000);
 
 client.login(token);
 
